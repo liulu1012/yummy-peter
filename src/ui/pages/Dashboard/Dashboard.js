f9d2a764375201fb36cfad39f33dac6070229506
liulu1012 <645911726@qq.com>
2017-07-30 10:26:00 +0000

    feed header

--- src/ui/pages/Dashboard/Dashboard.js
+++ src/ui/pages/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TitleHeader from '../../shared/TitleHeader/TitleHeader'
 import './dashboard.css'
+import svg from './comment.svg'
 
 class Dashboard extends React.Component{
 	render(){
@@ -8,7 +9,24 @@ class Dashboard extends React.Component{
 			<div className='dashboard'>
 				<TitleHeader title='好友更新' />
 				<div className='feed-wrap'>
-					<div className='feed-item'></div>
+					<div className='feed-item'>
+            <div className='feed-item-header'>
+              <div className='feed-user'>
+                <img src='http://media.haoduoshipin.com/yummy/default-avatar.png' alt='avtar'/>
+                <div className='feed-user-name-wrap'>
+                  <div className='feed-user-name'>
+                    liulu
+                  </div>
+                  <div className='feed-user-time'>
+                    2017nian7yue
+                  </div>
+                </div>
+              </div>
+              <div className='feed-comment-btn'>
+                <img src={svg} alt='comment' />
+              </div>
+            </div>
+          </div>
 				</div>
 			</div>
 		)
